dd4d42fd316ea1a771c2f3a42a944ca1bb01d6ab
Aiden Vigue <acvigue@me.com>
2021-06-14 16:33:17 -0400

    fix(backend): force same device id

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -190,7 +190,7 @@ authRoutes.post('/jellyfin', async (req, res, next) => {
       deviceId = user.jellyfinDeviceId ?? '';
     } else {
       deviceId = Buffer.from(
-        `Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0|${Date.now()}`
+        `Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0`
       ).toString('base64');
     }
     // First we need to attempt to log the user in to jellyfin
