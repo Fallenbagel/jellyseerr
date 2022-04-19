3a010f821189414efd334b4cad2a300501f40a18
notfakie <103784113+notfakie@users.noreply.github.com>
2022-04-19 12:17:47 +1200

    fix: relax jellyfin url validation to allow local domains

--- src/components/Login/JellyfinLogin.tsx
+++ src/components/Login/JellyfinLogin.tsx
@@ -48,7 +48,10 @@ const JellyfinLogin: React.FC<JellyfinLoginProps> = ({
   if (initial) {
     const LoginSchema = Yup.object().shape({
       host: Yup.string()
-        .url(intl.formatMessage(messages.validationhostformat))
+        .matches(
+          /^(?:(?:(?:https?):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/,
+          intl.formatMessage(messages.validationhostformat)
+        )
         .required(intl.formatMessage(messages.validationhostrequired)),
       email: Yup.string()
         .email(intl.formatMessage(messages.validationemailformat))
