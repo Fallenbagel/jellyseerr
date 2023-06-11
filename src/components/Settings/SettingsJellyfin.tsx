e848386d10f05f157e7a6dde8847ecab50c169ac
Fallenbagel <98979876+Fallenbagel@users.noreply.github.com>
2023-06-11 05:15:26 +0500

    fix: fix regex for internal url to use a more effecient one

--- src/components/Settings/SettingsJellyfin.tsx
+++ src/components/Settings/SettingsJellyfin.tsx
@@ -91,7 +91,7 @@ const SettingsJellyfin: React.FC<SettingsJellyfinProps> = ({
       intl.formatMessage(messages.validationUrl)
     ),
     jellyfinInternalUrl: Yup.string().matches(
-      /^(?:(?:(?:https?):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/,
+      /^(https?:\/\/)?(?:[\w-]+\.)*[\w-]+(?::\d{2,5})?(?:\/[\w-]+)*(?:\/)?$/gm,
       intl.formatMessage(messages.validationUrl)
     ),
   });
