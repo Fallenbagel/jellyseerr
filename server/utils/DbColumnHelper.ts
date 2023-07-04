f5596b799ddf07fc97fb6a3ebb8591b9fbd636a3
zackhow <zackhow@gmail.com>
2023-07-04 17:30:51 -0400

    feat: support for postgresql

---
+++ server/utils/DbColumnHelper.ts
@@ -0,0 +1,21 @@
+import { isPgsql } from '@server/datasource';
+import type { ColumnOptions, ColumnType } from 'typeorm';
+import { Column } from 'typeorm';
+
+const pgTypeMapping: { [key: string]: ColumnType } = {
+  datetime: 'timestamp with time zone',
+};
+
+export function resolveDbType(pgType: ColumnType): ColumnType {
+  if (isPgsql && pgType.toString() in pgTypeMapping) {
+    return pgTypeMapping[pgType.toString()];
+  }
+  return pgType;
+}
+
+export function DbAwareColumn(columnOptions: ColumnOptions) {
+  if (columnOptions.type) {
+    columnOptions.type = resolveDbType(columnOptions.type);
+  }
+  return Column(columnOptions);
+}
