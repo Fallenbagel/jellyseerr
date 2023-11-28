01665c47bb8cc87616ba119660a431acb2f5f9ce
Jānis Jansons <janhouse@gmail.com>
2023-11-28 21:59:53 +0200

    feat: add forward-auth support

--- src/pages/tv/[tvId]/index.tsx
+++ src/pages/tv/[tvId]/index.tsx
@@ -1,4 +1,5 @@
 import TvDetails from '@app/components/TvDetails';
+import { getRequestHeaders } from '@app/utils/localRequestHelper';
 import type { TvDetails as TvDetailsType } from '@server/models/Tv';
 import type { GetServerSideProps, NextPage } from 'next';
 
@@ -16,9 +17,7 @@ export const getServerSideProps: GetServerSideProps<TvPageProps> = async (
   const res = await fetch(
     `http://localhost:${process.env.PORT || 5055}/api/v1/tv/${ctx.query.tvId}`,
     {
-      headers: ctx.req?.headers?.cookie
-        ? { cookie: ctx.req.headers.cookie }
-        : undefined,
+      headers: getRequestHeaders(ctx),
     }
   );
   if (!res.ok) throw new Error();
