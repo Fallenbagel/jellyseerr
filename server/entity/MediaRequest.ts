eb111ac1db4728870522c2f49f902d3352ca8e9a
dr-carrot <alomba2014@gmail.com>
2024-01-22 17:34:02 -0500

    fix: removed psql specific relation checks

--- server/entity/MediaRequest.ts
+++ server/entity/MediaRequest.ts
@@ -12,7 +12,7 @@ import {
   MediaStatus,
   MediaType,
 } from '@server/constants/media';
-import { getRepository, isPgsql } from '@server/datasource';
+import { getRepository } from '@server/datasource';
 import type { MediaRequestBody } from '@server/interfaces/api/requestInterfaces';
 import notificationManager, { Notification } from '@server/lib/notifications';
 import { Permission } from '@server/lib/permissions';
@@ -569,13 +569,6 @@ export class MediaRequest {
       });
       return;
     }
-    // Typeorm pgsql doesn't load this relation even though the query specifically
-    // calls for it. The fk will get blown away from media_request when it is
-    // saved. Adding request back in prior to saving.
-    if (isPgsql) {
-      media.requests.push(this);
-    }
-
     const seasonRequestRepository = getRepository(SeasonRequest);
     if (
       this.status === MediaRequestStatus.APPROVED &&
@@ -633,13 +626,6 @@ export class MediaRequest {
       where: { id: this.media.id },
       relations: { requests: true },
     });
-
-    // Typeorm pgsql doesn't load this relation even though the query specifically
-    // calls for it. The fk will get blown away from media_request when it is
-    // saved. Adding request back in prior to saving.
-    if (isPgsql) {
-      fullMedia.requests.push(this);
-    }
 
     if (
       !fullMedia.requests.some((request) => !request.is4k) &&
@@ -862,7 +848,7 @@ export class MediaRequest {
             const requestRepository = getRepository(MediaRequest);
 
             this.status = MediaRequestStatus.FAILED;
-            requestRepository.save(this);
+            await requestRepository.save(this);
 
             logger.warn(
               'Something went wrong sending movie request to Radarr, marking status as FAILED',
@@ -1143,7 +1129,7 @@ export class MediaRequest {
             const requestRepository = getRepository(MediaRequest);
 
             this.status = MediaRequestStatus.FAILED;
-            requestRepository.save(this);
+            await requestRepository.save(this);
 
             logger.warn(
               'Something went wrong sending series request to Sonarr, marking status as FAILED',
