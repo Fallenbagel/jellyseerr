cc7b2250329cdc24c268c0894c14c8dd9f592a72
fallenbagel <98979876+Fallenbagel@users.noreply.github.com>
2024-05-23 11:07:25 +0500

    fix(error): combine auth and api error class into a single one called network error

--- server/types/error.ts
+++ server/types/error.ts
@@ -1,17 +1,12 @@
 import type { ApiErrorCode, AuthErrorCode } from '@server/constants/error';
 
-export class ApiError extends Error {
-  constructor(public statusCode: number, public errorCode: ApiErrorCode) {
+export class NetworkError extends Error {
+  constructor(
+    public statusCode: number,
+    public errorCode: ApiErrorCode | AuthErrorCode
+  ) {
     super();
 
     this.name = 'apiError';
-  }
-}
-
-export class AuthError extends Error {
-  constructor(public statusCode: number, public errorCode: AuthErrorCode) {
-    super();
-
-    this.name = 'authError';
   }
 }
