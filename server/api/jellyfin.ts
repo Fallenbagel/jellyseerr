Fallenbagel/jellyseerr

commit d765055da83ee94546399f6348aee14d8427d462

feat(auth): send real information on login (#470)
* feat(auth): send real ip on login

* feat(auth): send application name on login

server/api/jellyfin.ts
@@ -104,9 +104,9 @@ class JellyfinAPI {
 
     let authHeaderVal = '';
     if (this.authToken) {
-      authHeaderVal = `MediaBrowser Client="Overseerr", Device="Axios", DeviceId="${deviceId}", Version="10.8.0", Token="${authToken}"`;
+      authHeaderVal = `MediaBrowser Client="Jellyseerr", Device="Jellyseerr", DeviceId="${deviceId}", Version="10.8.0", Token="${authToken}"`;
     } else {
-      authHeaderVal = `MediaBrowser Client="Overseerr", Device="Axios", DeviceId="${deviceId}", Version="10.8.0"`;
+      authHeaderVal = `MediaBrowser Client="Jellyseerr", Device="Jellyseerr", DeviceId="${deviceId}", Version="10.8.0"`;
     }
 
     this.axios = axios.create({
@@ -121,14 +121,23 @@ class JellyfinAPI {
 
   public async login(
     Username?: string,
-    Password?: string
+    Password?: string,
+    ClientIP?: string
   ): Promise<JellyfinLoginResponse> {
     try {
+      const headers = ClientIP
+        ? {
+            'X-Forwarded-For': ClientIP,
+          }
+        : {};
       const account = await this.axios.post<JellyfinLoginResponse>(
         '/Users/AuthenticateByName',
         {
           Username: Username,
           Pw: Password,
+        },
+        {
+          headers: headers,
         }
       );
 

server/routes/auth.ts
@@ -271,7 +271,13 @@ authRoutes.post('/jellyfin', async (req, res, next) => {
       ? jellyfinHost.slice(0, -1)
       : jellyfinHost;
 
-    const account = await jellyfinserver.login(body.username, body.password);
+    const ip = req.ip ? req.ip.split(':').reverse()[0] : undefined;
+    const account = await jellyfinserver.login(
+      body.username,
+      body.password,
+      ip
+    );
+
     // Next let's see if the user already exists
     user = await userRepository.findOne({
       where: { jellyfinUserId: account.User.Id },

commit 857844d0589e36ed43dd573d5eb0691f197e2cc9

refactor(authapi): temporarily log the auth error and the client ip
Attempt to debug any issues caused by #470

server/api/jellyfin.ts
@@ -126,6 +126,7 @@ class JellyfinAPI extends ExternalAPI {
     Password?: string,
     ClientIP?: string
   ): Promise<JellyfinLoginResponse> {
+    logger.debug('Client IP: ', ClientIP);
     try {
       const headers = ClientIP
         ? {
@@ -146,6 +147,7 @@ class JellyfinAPI extends ExternalAPI {
 
       return authResponse;
     } catch (e) {
+      logger.debug(e);
       const status = e.response?.status;
 
       const networkErrorCodes = new Set([