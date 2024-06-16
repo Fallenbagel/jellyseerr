2785a2fb1637cd442542be7519766d4adc09c9a1
Aidan Hilt <aidanhilt2@gmail.com>
2024-06-16 00:37:04 -0400

    feat: update

--- server/lib/settings/index.ts
+++ server/lib/settings/index.ts
@@ -610,6 +610,7 @@ class Settings {
 
   private generateApiKey(): string {
     if (process.env.API_KEY) {
+      console.log(process.env.API_KEY);
       return process.env.API_KEY;
     } else {
       return Buffer.from(`${Date.now()}${randomUUID()}`).toString('base64');
