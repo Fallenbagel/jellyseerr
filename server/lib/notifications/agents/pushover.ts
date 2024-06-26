0f6ba544f85e77570044c0050e766680bd297da8
gauthier-th <mail@gauthierth.fr>
2024-06-26 16:35:46 +0200

    refactor: switch server from axios to fetch api

--- server/lib/notifications/agents/pushover.ts
+++ server/lib/notifications/agents/pushover.ts
@@ -5,7 +5,6 @@ import { User } from '@server/entity/User';
 import type { NotificationAgentPushover } from '@server/lib/settings';
 import { getSettings, NotificationAgentKey } from '@server/lib/settings';
 import logger from '@server/logger';
-import axios from 'axios';
 import {
   hasNotificationType,
   Notification,
@@ -52,12 +51,12 @@ class PushoverAgent
     imageUrl: string
   ): Promise<Partial<PushoverImagePayload>> {
     try {
-      const response = await axios.get(imageUrl, {
-        responseType: 'arraybuffer',
-      });
-      const base64 = Buffer.from(response.data, 'binary').toString('base64');
+      const response = await fetch(imageUrl);
+      const arrayBuffer = await response.arrayBuffer();
+      const base64 = Buffer.from(arrayBuffer).toString('base64');
       const contentType = (
-        response.headers['Content-Type'] || response.headers['content-type']
+        response.headers.get('Content-Type') ||
+        response.headers.get('content-type')
       )?.toString();
 
       return {
@@ -201,12 +200,18 @@ class PushoverAgent
       });
 
       try {
-        await axios.post(endpoint, {
-          ...notificationPayload,
-          token: settings.options.accessToken,
-          user: settings.options.userToken,
-          sound: settings.options.sound,
-        } as PushoverPayload);
+        await fetch(endpoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            ...notificationPayload,
+            token: settings.options.accessToken,
+            user: settings.options.userToken,
+            sound: settings.options.sound,
+          } as PushoverPayload),
+        });
       } catch (e) {
         logger.error('Error sending Pushover notification', {
           label: 'Notifications',
@@ -241,12 +246,18 @@ class PushoverAgent
         });
 
         try {
-          await axios.post(endpoint, {
-            ...notificationPayload,
-            token: payload.notifyUser.settings.pushoverApplicationToken,
-            user: payload.notifyUser.settings.pushoverUserKey,
-            sound: payload.notifyUser.settings.pushoverSound,
-          } as PushoverPayload);
+          await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              ...notificationPayload,
+              token: payload.notifyUser.settings.pushoverApplicationToken,
+              user: payload.notifyUser.settings.pushoverUserKey,
+              sound: payload.notifyUser.settings.pushoverSound,
+            } as PushoverPayload),
+          });
         } catch (e) {
           logger.error('Error sending Pushover notification', {
             label: 'Notifications',
@@ -291,11 +302,17 @@ class PushoverAgent
               });
 
               try {
-                await axios.post(endpoint, {
-                  ...notificationPayload,
-                  token: user.settings.pushoverApplicationToken,
-                  user: user.settings.pushoverUserKey,
-                } as PushoverPayload);
+                await fetch(endpoint, {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({
+                    ...notificationPayload,
+                    token: user.settings.pushoverApplicationToken,
+                    user: user.settings.pushoverUserKey,
+                  } as PushoverPayload),
+                });
               } catch (e) {
                 logger.error('Error sending Pushover notification', {
                   label: 'Notifications',
