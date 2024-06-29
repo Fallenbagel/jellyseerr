eeb72f3c88e3604a58abea94786730039df7191f
fallenbagel <98979876+Fallenbagel@users.noreply.github.com>
2024-06-29 09:06:24 +0500

    ci: override format checking for pnpm-lock in gen-docs folder

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,12 @@ module.exports = {
   plugins: [require('./merged-prettier-plugin.js')],
   singleQuote: true,
   trailingComma: 'es5',
+  overrides: [
+    {
+      files: 'gen-docs/pnpm-lock.yaml',
+      options: {
+        rangeEnd: 0, // default: Infinity
+      },
+    },
+  ],
 };
