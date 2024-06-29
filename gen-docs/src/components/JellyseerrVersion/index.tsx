42beb4c0b7e7f328fed132795b80cda79b179c8a
fallenbagel <98979876+Fallenbagel@users.noreply.github.com>
2024-06-30 03:06:32 +0500

    docs: nixpkg version check component

--- gen-docs/src/components/JellyseerrVersion/index.tsx
+++ gen-docs/src/components/JellyseerrVersion/index.tsx
@@ -1,27 +1,67 @@
 import { useEffect, useState } from 'react';
 
-function JellyseerrVersion() {
+export const JellyseerrVersion = () => {
   const [version, setVersion] = useState<string | null>('0.0.0');
 
   useEffect(() => {
     async function fetchVersion() {
       try {
         const response = await fetch(
-          'https://api.github.com/repos/Fallenbagel/jellyseerr/releases/latest'
+          'https://raw.githubusercontent.com/Fallenbagel/jellyseerr/main/package.json'
         );
-        const data = await response.json();
-        const latestVersion = data.tag_name.replace('v', '');
 
-        setVersion(latestVersion);
+        const data = await response.json();
+
+        setVersion(data.version);
+        console.log(data.version);
       } catch (error) {
-        console.error('Failed to fetch version hash:', error);
-        setVersion('Error fetching version hash');
+        console.error('Failed to fetch version', error);
+        setVersion('Error fetching version');
       }
     }
     fetchVersion();
   }, []);
 
   return version;
-}
+};
 
-export default JellyseerrVersion;
+export const NixpkgVersion = () => {
+  const [version, setVersion] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchVersion = async () => {
+      try {
+        const url =
+          'https://raw.githubusercontent.com/NixOS/nixpkgs/nixos-unstable/pkgs/servers/jellyseerr/default.nix';
+        const response = await fetch(url);
+        const data = await response.text();
+
+        const versionRegex = /version\s*=\s*"([^"]+)"/;
+        const match = data.match(versionRegex);
+        if (match && match[1]) {
+          setVersion(match[1]);
+        } else {
+          setError('0.0.0');
+        }
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchVersion();
+  }, []);
+
+  if (loading) {
+    return 'Loading...';
+  }
+
+  if (error) {
+    return { error };
+  }
+
+  return version;
+};
