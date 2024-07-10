c4054531cc645877a09daebd766fff1ca18001e1
Gauthier <mail@gauthierth.fr>
2024-07-10 14:31:01 +0200

    fix: try to fix network errors with IPv6

--- server/index.ts
+++ server/index.ts
@@ -32,9 +32,14 @@ import * as OpenApiValidator from 'express-openapi-validator';
 import type { Store } from 'express-session';
 import session from 'express-session';
 import next from 'next';
+import dns from 'node:dns';
+import net from 'node:net';
 import path from 'path';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
+
+dns.setDefaultResultOrder('ipv4first');
+net.setDefaultAutoSelectFamily(false);
 
 const API_SPEC_PATH = path.join(__dirname, '../overseerr-api.yml');
 
