c3a6c7d4b202d8507b2e74754736b0a9de063950
fallenbagel <98979876+Fallenbagel@users.noreply.github.com>
2024-07-25 19:39:25 +0500

    fix: emby media server type migration

--- server/lib/settings/migrations/0002_emby_media_server_type.ts
+++ server/lib/settings/migrations/0002_emby_media_server_type.ts
@@ -1,9 +1,10 @@
 import type { AllSettings } from '@server/lib/settings';
 
 const migrateHostname = (settings: any): AllSettings => {
-  const oldMediaServerType = settings.mediaServerType;
+  const oldMediaServerType = settings.main.mediaServerType;
+  console.log('Migrating media server type', oldMediaServerType);
   if (oldMediaServerType === 2 && process.env.JELLYFIN_TYPE === 'emby') {
-    settings.mediaServerType = 3;
+    settings.main.mediaServerType = 3;
   }
 
   return settings;
