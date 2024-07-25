e03edd2d1f0c69fb228fe4985c26c55b3f996649
fallenbagel <98979876+Fallenbagel@users.noreply.github.com>
2024-07-26 03:38:11 +0500

    refactor: use enums instead of numbers

--- server/lib/settings/migrations/0002_emby_media_server_type.ts
+++ server/lib/settings/migrations/0002_emby_media_server_type.ts
@@ -1,10 +1,14 @@
+import { MediaServerType } from '@server/constants/server';
 import type { AllSettings } from '@server/lib/settings';
 
 const migrateHostname = (settings: any): AllSettings => {
   const oldMediaServerType = settings.main.mediaServerType;
   console.log('Migrating media server type', oldMediaServerType);
-  if (oldMediaServerType === 2 && process.env.JELLYFIN_TYPE === 'emby') {
-    settings.main.mediaServerType = 3;
+  if (
+    oldMediaServerType === MediaServerType.JELLYFIN &&
+    process.env.JELLYFIN_TYPE === 'emby'
+  ) {
+    settings.main.mediaServerType = MediaServerType.EMBY;
   }
 
   return settings;
