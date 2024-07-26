7e2ec24441878807880d5b812924167be8282155
TOomaAh <tbrunet72@gmail.com>
2024-07-27 01:28:28 +0200

    fix(usersettings): remove unused column tvdbtoken

--- server/entity/UserSettings.ts
+++ server/entity/UserSettings.ts
@@ -40,9 +40,6 @@ export class UserSettings {
   public originalLanguage?: string;
 
   @Column({ nullable: true })
-  public tvdbToken?: string;
-
-  @Column({ nullable: true })
   public pgpKey?: string;
 
   @Column({ nullable: true })
