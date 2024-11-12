655efc46e1f63179144403170b5fe866797db9d6
Michael Thomas <michaelhthomas@outlook.com>
2024-11-12 10:50:23 -0500

    style(usersettings): improve syntax & performance of user password checks

--- server/routes/user/usersettings.ts
+++ server/routes/user/usersettings.ts
@@ -366,9 +366,13 @@ userSettingsRoutes.delete<{ id: string }>(
     }
 
     try {
-      const user = await userRepository.findOne({
-        where: { id: Number(req.params.id) },
-      });
+      const user = await userRepository
+        .createQueryBuilder('user')
+        .addSelect('user.password')
+        .where({
+          id: Number(req.params.id),
+        })
+        .getOne();
 
       if (!user) {
         return res.status(404).json({ message: 'User not found.' });
@@ -381,14 +385,7 @@ userSettingsRoutes.delete<{ id: string }>(
         });
       }
 
-      const hasPassword = !!(
-        await userRepository.findOne({
-          where: { id: user.id },
-          select: ['id', 'password'],
-        })
-      )?.password;
-
-      if (!user.email || !hasPassword) {
+      if (!user.email || !user.password) {
         return res.status(400).json({
           message: 'User does not have a local email or password set.',
         });
@@ -524,9 +521,13 @@ userSettingsRoutes.delete<{ id: string }>(
     }
 
     try {
-      const user = await userRepository.findOne({
-        where: { id: Number(req.params.id) },
-      });
+      const user = await userRepository
+        .createQueryBuilder('user')
+        .addSelect('user.password')
+        .where({
+          id: Number(req.params.id),
+        })
+        .getOne();
 
       if (!user) {
         return res.status(404).json({ message: 'User not found.' });
@@ -539,14 +540,7 @@ userSettingsRoutes.delete<{ id: string }>(
         });
       }
 
-      const hasPassword = !!(
-        await userRepository.findOne({
-          where: { id: user.id },
-          select: ['id', 'password'],
-        })
-      )?.password;
-
-      if (!user.email || !hasPassword) {
+      if (!user.email || !user.password) {
         return res.status(400).json({
           message: 'User does not have a local email or password set.',
         });
