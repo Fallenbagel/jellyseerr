7e78eeee5845d640a28c8024a5979137ea228219
Ben Haney <ben@benhaney.com>
2024-12-20 00:48:01 -0600

    fix: handle non-existent rottentomatoes rating for movies
    This fixes a bug where some movies don't have any rottentomatoes
    ratings, which causes ratings from other services to not show

--- server/api/rating/rottentomatoes.ts
+++ server/api/rating/rottentomatoes.ts
@@ -128,7 +128,7 @@ class RottenTomatoes extends ExternalAPI {
         movie = contentResults.hits.find((movie) => movie.title === name);
       }
 
-      if (!movie) {
+      if (!movie?.rottenTomatoes) {
         return null;
       }
 
