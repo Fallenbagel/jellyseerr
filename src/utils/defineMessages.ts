c1ea7673388e215fb1244e02ac0c1ef1bc650e3d
Gauthier <mail@gauthierth.fr>
2024-12-28 22:08:01 +0100

    fix: remove unrelated changes to the PR

--- src/utils/defineMessages.ts
+++ src/utils/defineMessages.ts
@@ -1,26 +1,18 @@
 import { defineMessages as intlDefineMessages } from 'react-intl';
 
-type Messages<T extends Record<string, string>> = {
-  [K in keyof T]: {
-    id: string;
-    defaultMessage: T[K];
-  };
-};
-
-export default function defineMessages<T extends Record<string, string>>(
+export default function defineMessages(
   prefix: string,
-  messages: T
-): Messages<T> {
-  const keys: (keyof T)[] = Object.keys(messages);
-  const modifiedMessagesEntries = keys.map((key) => [
-    key,
-    {
-      id: `${prefix}.${key as string}`,
+  messages: Record<string, string>
+) {
+  const modifiedMessages: Record<
+    string,
+    { id: string; defaultMessage: string }
+  > = {};
+  for (const key of Object.keys(messages)) {
+    modifiedMessages[key] = {
+      id: prefix + '.' + key,
       defaultMessage: messages[key],
-    },
-  ]);
-  const modifiedMessages: Messages<T> = Object.fromEntries(
-    modifiedMessagesEntries
-  );
+    };
+  }
   return intlDefineMessages(modifiedMessages);
 }
