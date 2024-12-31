Fallenbagel/jellyseerr

commit b6e2e6ce615cb94cea8d2335140fe245a0ca2d8a

feat: add a setting for special episodes (#1193)
* feat: add a setting for special episodes

This PR adds a separate setting for special episodes and disables them by default, to avoid unwanted
library status updates.

* refactor(settings): re-order setting for allow specials request

---------

Co-authored-by: fallenbagel <98979876+Fallenbagel@users.noreply.github.com>

server/entity/MediaRequest.ts
@@ -58,6 +58,7 @@ export class MediaRequest {
     const mediaRepository = getRepository(Media);
     const requestRepository = getRepository(MediaRequest);
     const userRepository = getRepository(User);
+    const settings = getSettings();
 
     let requestUser = user;
 
@@ -258,7 +259,11 @@ export class MediaRequest {
       >;
       const requestedSeasons =
         requestBody.seasons === 'all'
-          ? tmdbMediaShow.seasons.map((season) => season.season_number)
+          ? settings.main.enableSpecialEpisodes
+            ? tmdbMediaShow.seasons.map((season) => season.season_number)
+            : tmdbMediaShow.seasons
+                .map((season) => season.season_number)
+                .filter((sn) => sn > 0)
           : (requestBody.seasons as number[]);
       let existingSeasons: number[] = [];
 

server/lib/scanners/sonarr/index.ts
@@ -102,9 +102,12 @@ class SonarrScanner
       }
 
       const tmdbId = tvShow.id;
+      const settings = getSettings();
 
-      const filteredSeasons = sonarrSeries.seasons.filter((sn) =>
-        tvShow.seasons.find((s) => s.season_number === sn.seasonNumber)
+      const filteredSeasons = sonarrSeries.seasons.filter(
+        (sn) =>
+          tvShow.seasons.find((s) => s.season_number === sn.seasonNumber) &&
+          (!settings.main.partialRequestsEnabled ? sn.seasonNumber !== 0 : true)
       );
 
       for (const season of filteredSeasons) {

src/components/RequestModal/TvRequestModal.tsx
@@ -253,9 +253,13 @@ const TvRequestModal = ({
   };
 
   const getAllSeasons = (): number[] => {
-    return (data?.seasons ?? [])
-      .filter((season) => season.episodeCount !== 0)
-      .map((season) => season.seasonNumber);
+    let allSeasons = (data?.seasons ?? []).filter(
+      (season) => season.episodeCount !== 0
+    );
+    if (!settings.currentSettings.partialRequestsEnabled) {
+      allSeasons = allSeasons.filter((season) => season.seasonNumber !== 0);
+    }
+    return allSeasons.map((season) => season.seasonNumber);
   };
 
   const getAllRequestedSeasons = (): number[] => {
@@ -577,7 +581,12 @@ const TvRequestModal = ({
                 </thead>
                 <tbody className="divide-y divide-gray-700">
                   {data?.seasons
-                    .filter((season) => season.episodeCount !== 0)
+                    .filter(
+                      (season) =>
+                        (!settings.currentSettings.enableSpecialEpisodes
+                          ? season.seasonNumber !== 0
+                          : true) && season.episodeCount !== 0
+                    )
                     .map((season) => {
                       const seasonRequest = getSeasonRequest(
                         season.seasonNumber

src/components/TvDetails/index.tsx
@@ -301,7 +301,9 @@ const TvDetails = ({ tv }: TvDetailsProps) => {
   };
 
   const showHasSpecials = data.seasons.some(
-    (season) => season.seasonNumber === 0
+    (season) =>
+      season.seasonNumber === 0 &&
+      settings.currentSettings.partialRequestsEnabled
   );
 
   const isComplete =
@@ -799,6 +801,11 @@ const TvDetails = ({ tv }: TvDetailsProps) => {
             {data.seasons
               .slice()
               .reverse()
+              .filter(
+                (season) =>
+                  settings.currentSettings.enableSpecialEpisodes ||
+                  season.seasonNumber !== 0
+              )
               .map((season) => {
                 const show4k =
                   settings.currentSettings.series4kEnabled &&

server/lib/scanners/plex/index.ts
@@ -277,8 +277,13 @@ class PlexScanner
 
     const seasons = tvShow.seasons;
     const processableSeasons: ProcessableSeason[] = [];
+    const settings = getSettings();
 
-    for (const season of seasons) {
+    const filteredSeasons = settings.main.enableSpecialEpisodes
+      ? seasons
+      : seasons.filter((sn) => sn.season_number !== 0);
+
+    for (const season of filteredSeasons) {
       const matchedPlexSeason = metadata.Children?.Metadata.find(
         (md) => Number(md.index) === season.season_number
       );

server/lib/settings/index.ts
@@ -131,6 +131,7 @@ export interface MainSettings {
   trustProxy: boolean;
   mediaServerType: number;
   partialRequestsEnabled: boolean;
+  enableSpecialEpisodes: boolean;
   locale: string;
   proxy: ProxySettings;
 }
@@ -154,6 +155,7 @@ interface FullPublicSettings extends PublicSettings {
   jellyfinForgotPasswordUrl?: string;
   jellyfinServerName?: string;
   partialRequestsEnabled: boolean;
+  enableSpecialEpisodes: boolean;
   cacheImages: boolean;
   vapidPublic: string;
   enablePushRegistration: boolean;
@@ -343,6 +345,7 @@ class Settings {
         trustProxy: false,
         mediaServerType: MediaServerType.NOT_CONFIGURED,
         partialRequestsEnabled: true,
+        enableSpecialEpisodes: false,
         locale: 'en',
         proxy: {
           enabled: false,
@@ -587,6 +590,7 @@ class Settings {
       originalLanguage: this.data.main.originalLanguage,
       mediaServerType: this.main.mediaServerType,
       partialRequestsEnabled: this.data.main.partialRequestsEnabled,
+      enableSpecialEpisodes: this.data.main.enableSpecialEpisodes,
       cacheImages: this.data.main.cacheImages,
       vapidPublic: this.vapidPublic,
       enablePushRegistration: this.data.notifications.agents.webpush.enabled,

src/components/RequestCard/index.tsx
@@ -5,6 +5,7 @@ import Tooltip from '@app/components/Common/Tooltip';
 import RequestModal from '@app/components/RequestModal';
 import StatusBadge from '@app/components/StatusBadge';
 import useDeepLinks from '@app/hooks/useDeepLinks';
+import useSettings from '@app/hooks/useSettings';
 import { Permission, useUser } from '@app/hooks/useUser';
 import globalMessages from '@app/i18n/globalMessages';
 import defineMessages from '@app/utils/defineMessages';
@@ -219,6 +220,7 @@ interface RequestCardProps {
 }
 
 const RequestCard = ({ request, onTitleData }: RequestCardProps) => {
+  const settings = useSettings();
   const { ref, inView } = useInView({
     triggerOnce: true,
   });
@@ -411,7 +413,11 @@ const RequestCard = ({ request, onTitleData }: RequestCardProps) => {
               <span className="mr-2 font-bold ">
                 {intl.formatMessage(messages.seasons, {
                   seasonCount:
-                    title.seasons.length === request.seasons.length
+                    (settings.currentSettings.enableSpecialEpisodes
+                      ? title.seasons.length
+                      : title.seasons.filter(
+                          (season) => season.seasonNumber !== 0
+                        ).length) === request.seasons.length
                       ? 0
                       : request.seasons.length,
                 })}

src/components/RequestList/RequestItem/index.tsx
@@ -5,6 +5,7 @@ import ConfirmButton from '@app/components/Common/ConfirmButton';
 import RequestModal from '@app/components/RequestModal';
 import StatusBadge from '@app/components/StatusBadge';
 import useDeepLinks from '@app/hooks/useDeepLinks';
+import useSettings from '@app/hooks/useSettings';
 import { Permission, useUser } from '@app/hooks/useUser';
 import globalMessages from '@app/i18n/globalMessages';
 import defineMessages from '@app/utils/defineMessages';
@@ -294,6 +295,7 @@ interface RequestItemProps {
 }
 
 const RequestItem = ({ request, revalidateList }: RequestItemProps) => {
+  const settings = useSettings();
   const { ref, inView } = useInView({
     triggerOnce: true,
   });
@@ -481,7 +483,11 @@ const RequestItem = ({ request, revalidateList }: RequestItemProps) => {
                   <span className="card-field-name">
                     {intl.formatMessage(messages.seasons, {
                       seasonCount:
-                        title.seasons.length === request.seasons.length
+                        (settings.currentSettings.enableSpecialEpisodes
+                          ? title.seasons.length
+                          : title.seasons.filter(
+                              (season) => season.seasonNumber !== 0
+                            ).length) === request.seasons.length
                           ? 0
                           : request.seasons.length,
                     })}

commit 6b1f25bab9b51e5cc8130a5bd76b1306c01f6f22

fix: correct typos for the special episodes setting
Some typos were introduced by #1193, enableSpecialEpisodes and partialRequestsEnabled were mixed up.

fix #1208

server/entity/MediaRequest.ts
@@ -386,14 +386,14 @@ export class MediaRequest {
       const tmdbMediaShow = tmdbMedia as Awaited<
         ReturnType<typeof tmdb.getTvShow>
       >;
-      const requestedSeasons =
+      let requestedSeasons =
         requestBody.seasons === 'all'
-          ? settings.main.enableSpecialEpisodes
-            ? tmdbMediaShow.seasons.map((season) => season.season_number)
-            : tmdbMediaShow.seasons
-                .map((season) => season.season_number)
-                .filter((sn) => sn > 0)
+          ? tmdbMediaShow.seasons.map((season) => season.season_number)
           : (requestBody.seasons as number[]);
+      if (!settings.main.enableSpecialEpisodes) {
+        requestedSeasons = requestedSeasons.filter((sn) => sn > 0);
+      }
+
       let existingSeasons: number[] = [];
 
       // We need to check existing requests on this title to make sure we don't double up on seasons that were

server/lib/scanners/sonarr/index.ts
@@ -107,7 +107,7 @@ class SonarrScanner
       const filteredSeasons = sonarrSeries.seasons.filter(
         (sn) =>
           tvShow.seasons.find((s) => s.season_number === sn.seasonNumber) &&
-          (!settings.main.partialRequestsEnabled ? sn.seasonNumber !== 0 : true)
+          (!settings.main.enableSpecialEpisodes ? sn.seasonNumber !== 0 : true)
       );
 
       for (const season of filteredSeasons) {

src/components/RequestModal/TvRequestModal.tsx
@@ -256,8 +256,8 @@ const TvRequestModal = ({
     let allSeasons = (data?.seasons ?? []).filter(
       (season) => season.episodeCount !== 0
     );
-    if (!settings.currentSettings.partialRequestsEnabled) {
-      allSeasons = allSeasons.filter((season) => season.seasonNumber !== 0);
+    if (!settings.currentSettings.enableSpecialEpisodes) {
+      allSeasons = allSeasons.filter((season) => season.seasonNumber > 0);
     }
     return allSeasons.map((season) => season.seasonNumber);
   };

src/components/TvDetails/index.tsx
@@ -303,7 +303,7 @@ const TvDetails = ({ tv }: TvDetailsProps) => {
   const showHasSpecials = data.seasons.some(
     (season) =>
       season.seasonNumber === 0 &&
-      settings.currentSettings.partialRequestsEnabled
+      settings.currentSettings.enableSpecialEpisodes
   );
 
   const isComplete =