ac908026dbb7ca06c0fb520bbb360120d6b87feb
Gauthier <mail@gauthierth.fr>
2025-01-02 16:03:45 +0100

    fix(jellyfinlogin): add proper error message when no admin user exists (#1216)
    This PR adds an error message when the database has no admin user and Jellyseerr has already been
    set up (i.e. settings.json is filled in), instead of having a generic error message.

--- server/constants/error.ts
+++ server/constants/error.ts
@@ -4,6 +4,7 @@ export enum ApiErrorCode {
   InvalidAuthToken = 'INVALID_AUTH_TOKEN',
   InvalidEmail = 'INVALID_EMAIL',
   NotAdmin = 'NOT_ADMIN',
+  NoAdminUser = 'NO_ADMIN_USER',
   SyncErrorGroupedFolders = 'SYNC_ERROR_GROUPED_FOLDERS',
   SyncErrorNoLibraries = 'SYNC_ERROR_NO_LIBRARIES',
   Unknown = 'UNKNOWN',
