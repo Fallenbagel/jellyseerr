fba5dbfd04822d1ec1ec78b5af3c43c19efdf4cb
Bryan J. <132493975+chkpwd@users.noreply.github.com>
2025-02-13 01:04:38 -0500

    docs: update error message for various components

--- src/components/MovieDetails/index.tsx
+++ src/components/MovieDetails/index.tsx
@@ -102,7 +102,7 @@ const messages = defineMessages('components.MovieDetails', {
   watchlistSuccess: '<strong>{title}</strong> added to watchlist successfully!',
   watchlistDeleted:
     '<strong>{title}</strong> Removed from watchlist successfully!',
-  watchlistError: 'Something went wrong try again.',
+  watchlistError: 'Something went wrong. Please try again.',
   removefromwatchlist: 'Remove From Watchlist',
   addtowatchlist: 'Add To Watchlist',
 });
